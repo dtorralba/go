ddb7a85bfd7b471d719a63163421bcd8333c6792
Diego Andres Torralba <diego.torralba@mercadolibre.com>
2018-12-07 12:18:25 -0300

    Ejercicio 12 interfaces cont ok

--- src/github.com/dtorralba/twitter/src/domain/tweet.go
+++ src/github.com/dtorralba/twitter/src/domain/tweet.go
@@ -12,6 +12,7 @@ type Tweet interface {
 	GetUser() string
 	GetText() string
 	GetDate() *time.Time
+	PrintableTweet() string
 }
 
 type TextTweet struct {
@@ -20,18 +21,48 @@ type TextTweet struct {
 	Date *time.Time
 }
 
-func NewTweet(user string, text string) TextTweet {
+type ImageTweet struct {
+	TextTweet
+	URL string
+}
+
+type QuoteTweet struct {
+	TextTweet
+	QuoteText TextTweet
+}
+
+func NewImageTweet(user string, text string, URL string) *ImageTweet {
+	textTweet := NewTextTweet(user, text)
+	imageTweet := ImageTweet{textTweet, URL}
+	return &imageTweet
+}
+
+func NewQuoteTweet(user string, text string, quote TextTweet) *QuoteTweet {
+	textTweet := NewTextTweet(user, text)
+	quoteTweet := QuoteTweet{textTweet, quote}
+	return &quoteTweet
+}
+
+func NewTextTweet(user string, text string) TextTweet {
 	var time = time.Now()
 	textTweet := TextTweet{user, text, &time}
 	return textTweet
 }
 
 func (textTweet *TextTweet) PrintableTweet() string {
-	return "@" + textTweet.User + ": " + textTweet.Text
+	return "@" + textTweet.GetUser() + ": " + textTweet.GetText()
+}
+
+func (imageTweet *ImageTweet) PrintableTweet() string {
+	return "@" + imageTweet.GetUser() + ": " + imageTweet.GetText() + " " + imageTweet.GetURL()
+}
+
+func (quoteTweet *QuoteTweet) PrintableTweet() string {
+	return "@" + quoteTweet.GetQuoteName() + ": " + quoteTweet.GetText() + " \"" + quoteTweet.GetQuoteText() + "\""
 }
 
 func (textTweet *TextTweet) String() string {
-	return "@" + textTweet.User + ": " + textTweet.Text
+	return "@" + textTweet.GetUser() + ": " + textTweet.GetText()
 }
 
 func (tweet TextTweet) GetUser() string {
@@ -44,4 +75,17 @@ func (tweet TextTweet) GetText() string {
 
 func (tweet TextTweet) GetDate() *time.Time {
 	return tweet.Date
+}
+
+func (imageTweet ImageTweet) GetURL() string {
+	return imageTweet.URL
+}
+
+func (quoteTweet QuoteTweet) GetQuoteName() string {
+	return quoteTweet.User
+}
+
+func (quoteTweet QuoteTweet) GetQuoteText() string {
+
+	return quoteTweet.QuoteText.PrintableTweet()
 }
--- src/github.com/dtorralba/twitter/src/domain_test/tweet_test.go
+++ src/github.com/dtorralba/twitter/src/domain_test/tweet_test.go
@@ -9,7 +9,7 @@ import (
 func TestTextTweetPrintsUserAndText(t *testing.T) {
 
 	// Initialization
-	tweet := domain.NewTweet("grupoesfera", "This is my tweet")
+	tweet := domain.NewTextTweet("grupoesfera", "This is my tweet")
 
 	// Operation
 	text := tweet.PrintableTweet()
@@ -25,7 +25,7 @@ func TestTextTweetPrintsUserAndText(t *testing.T) {
 func TestCanGetAStringFromATweet(t *testing.T) {
 
 	// Initialization
-	tweet := domain.NewTweet("grupoesfera", "This is my tweet")
+	tweet := domain.NewTextTweet("grupoesfera", "This is my tweet")
 
 	// Operation
 	text := tweet.String()
@@ -36,4 +36,34 @@ func TestCanGetAStringFromATweet(t *testing.T) {
 		t.Errorf("The expected text is %s but was %s", expectedText, text)
 	}
 
+}
+
+func TestImageTweetPrintsUserTextAndImageURL(t *testing.T) {
+
+	// Initialization
+	tweet := domain.NewImageTweet("grupoesfera", "This is my image", "http://www.grupoesfera.com.ar/common/img/grupoesfera.png")
+	// Operation
+	text := tweet.PrintableTweet()
+	// Validation
+	expectedText := "@grupoesfera: This is my image http://www.grupoesfera.com.ar/common/img/grupoesfera.png"
+	if text != expectedText {
+		t.Errorf("The expected text is %s but was %s", expectedText, text)
+	}
+
+}
+
+func TestQuoteTweetPrintsUserTextAndQuotedTweet(t *testing.T) {
+
+	// Initialization
+	quotedTweet := domain.NewTextTweet("grupoesfera", "This is my tweet")
+	tweet := domain.NewQuoteTweet("nick", "Awesome", quotedTweet)
+
+	//Operation
+	text := tweet.PrintableTweet()
+
+	// Validation
+	expectedText := `@nick: Awesome "@grupoesfera: This is my tweet"`
+	if text != expectedText {
+		t.Errorf("The expected text is %s but was %s", expectedText, text)
+	}
 }
